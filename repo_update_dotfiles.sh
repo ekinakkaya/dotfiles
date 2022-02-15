8c297c0ae9b92c25024981307d4da0a546935d43
ekinakkaya <ekoeko0654@gmail.com>
2022-02-15 15:08:12 +0300

    copy original files to dotfiles directory

---
+++ repo_update_dotfiles.sh
@@ -0,0 +1,9 @@
+# copy the dotfiles to this directory
+
+cp /mnt/c/Users/ekin_/AppData/Roaming/alacritty/alacritty.yml alacritty/alacritty.yml
+
+cp ~/.bashrc wsl-bash/.bashrc
+
+cp ~/.vimrc vim/.vimrc
+
+cp ~/.tmux.conf tmux/.tmux.conf
